dd4bdd3190481f4b1d6bee5bda1041052b44ebd9
Maxime Rouard <rouard.maxi@gmail.com>
2026-02-04 16:48:51 +0100

    little update to show user name

--- src/app/(without-home-layout)/service/page.tsx
+++ src/app/(without-home-layout)/service/page.tsx
@@ -1,10 +1,13 @@
 import LogOutButton from "@/app/components/log-out-button";
 import styles from "@/ui/styles/example.module.css"
+import {auth} from "@/auth";
 
-const ServicePage = () => {
+
+const ServicePage = async () => {
+    const session = await auth();
     return (
         <div className="w-full h-full flex flex-col items-center justify-center gap-20">
-            <p className={/* you can see here that text blue is overwritten by our styles modules*/`text-blue-500 font-bold ${styles.pCustom} `}> Welcome to the first service</p>
+            <p className={/* you can see here that text blue is overwritten by our styles modules*/`text-blue-500 font-bold ${styles.pCustom} `}> Welcome {session?.user?.name || ""} to the first service</p>
             <LogOutButton/>
         </div>
     )
--- src/app/(without-home-layout)/service2/page.tsx
+++ src/app/(without-home-layout)/service2/page.tsx
@@ -1,10 +1,13 @@
 import LogOutButton from "@/app/components/log-out-button";
 import styles from "@/ui/styles/example.module.css"
+import {auth} from "@/auth";
 
-const Service2Page = () => {
+const Service2Page = async () => {
+    const session = await auth();
     return (
         <div className="w-full h-full flex flex-col items-center justify-center gap-20">
-            <p className={/* you can see here that text blue is overwritten by our styles modules*/`text-blue-500 font-bold ${styles.pCustom} `}> Welcome to the second service</p>
+            <p className={/* you can see here that text blue is overwritten by our styles modules*/`text-blue-500 font-bold ${styles.pCustom} `}> Welcome {session?.user?.name || ""} to the second service</p>
+
             <LogOutButton/>
         </div>
     )
